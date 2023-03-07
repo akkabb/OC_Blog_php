b7a7ce274e7ebbcd1e2804b6c04b13e2103a9f54
akkabb <akkabb@gmx.fr>
2023-03-07 20:20:18 +0100

    folders Public + routes + file .htaccess for redirection

---
+++ Public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    use Router\Router;
+
+    require '../vendor/autoload.php';
+    
+    //echo $_GET['url'];
+
+     $router = new Router($_GET['url']);
+     $router->show();
+
+
+     ?>
